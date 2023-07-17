aeb74bb3dddeafa669045cadd50714f3f46d2e22
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-17 09:29:28 +0530

    frontend setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BroswerRouter,Navigate,Routes,Route } from 'react-router-dom'
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+
+function App() {
+  return (
+  <>
+  </>
+    );
+}
+
+export default App;

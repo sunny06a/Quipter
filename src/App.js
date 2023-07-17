af41c78dc2defcfc331fbd7bd76bdd0cf791d07d
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-17 11:10:47 +0530

    mui setup

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import {Navigate,Routes,Route,BrowserRouter } from 'react-router-dom'
 import HomePage from './scenes/homePage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
+import {useMemo} from 'react'
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider} from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 function App() {
+  const mode=useSelector((state)=> state.mode);
+  const theme=useMemo(()=> createTheme(themeSettings(mode)),[mode]);
   return (
   <>
   <BrowserRouter>
+  <ThemeProvider theme={theme}>
+  <CssBaseline/>
   <Routes>
     <Route path='/' element={<LoginPage></LoginPage>}></Route>
     <Route path='/home' element={<HomePage></HomePage>}></Route>
     <Route path='/profile/:userId' element={<ProfilePage></ProfilePage>}></Route>
   </Routes>
+  </ThemeProvider>
   </BrowserRouter>
 
 
--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Close, DarkMode, Help, LightMode, Menu, Message, Notifications, Search } from '@mui/icons-material';
+import { Box, FormControl, IconButton, InputBase, MenuItem, Select, Typography, useMediaQuery, useTheme } from '@mui/material';
+import FlexBetween from 'components/FlexBetween';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setLogout, setMode } from 'state';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    </>
   )
 }

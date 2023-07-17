41e7333d0e26a2227be269873950211a099a78ca
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-17 10:47:58 +0530

    redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import {BroswerRouter,Navigate,Routes,Route } from 'react-router-dom'
+//eslint-disable-next-line
+import {Navigate,Routes,Route,BrowserRouter } from 'react-router-dom'
 import HomePage from './scenes/homePage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
@@ -7,6 +8,15 @@ import ProfilePage from './scenes/profilePage';
 function App() {
   return (
   <>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<LoginPage></LoginPage>}></Route>
+    <Route path='/home' element={<HomePage></HomePage>}></Route>
+    <Route path='/profile/:userId' element={<ProfilePage></ProfilePage>}></Route>
+  </Routes>
+  </BrowserRouter>
+
+
   </>
     );
 }
